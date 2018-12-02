ee3150a33c8c9f6044e017f147cf7610960f4ca1
Unknown <rafik.laredj@laposte.net>
2018-12-02 22:20:49 +0100

    Mise en place du plugin react-diagrams
    disponible à l'url /#/flowchart, a faire : faire des tests avec des données réelles

--- flows-manager-ui/src/routes.js
+++ flows-manager-ui/src/routes.js
@@ -4,12 +4,14 @@ import { HashRouter as Router, Route } from 'react-router-dom'
 import App from './components/App'
 
 import Technos from './components/Technos'
+import FlowChart from './components/FlowChart'
 
 const Routes = (props) => (
   <Router {...props}>
     <div>
       <Route path='/' exact component={App} />
       <Route path='/technos' component={Technos} />
+      <Route path='/flowchart' component={FlowChart} />
     </div>
   </Router>
 )
