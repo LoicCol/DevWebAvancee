127528a724503b6503f64ca460bd594e379ead06
Loïc Colin @elamp <loic-col@hotmail.fr>
2019-01-05 18:09:49 +0100

    Bar Menu added

--- flows-manager-ui/src/routes.js
+++ flows-manager-ui/src/routes.js
@@ -1,5 +1,7 @@
 import React from 'react'
-import { HashRouter as Router, Route } from 'react-router-dom'
+import { HashRouter as Router, Route, Switch } from 'react-router-dom'
+import { Menubar } from 'primereact/menubar'
+import { Button } from 'primereact/button'
 
 import AppInit from './components/AppInit'
 
@@ -9,15 +11,54 @@ import FlowsContainer from './components/Flows/FlowsContainer'
 import FlowChart from './components/FlowChart'
 import AppsContainer from './components/Apps/AppsContainer'
 
+const menuItems = [
+  {
+    label: 'Technos',
+    icon: 'pi pi-fw pi-power-off',
+    command: event => {
+      window.location.hash = '#/technos'
+    }
+  },
+  {
+    label: 'Flows',
+    icon: 'pi pi-fw pi-power-off',
+    command: event => {
+      window.location.hash = '#/flows'
+    }
+  },
+  {
+    label: 'Apps',
+    icon: 'pi pi-fw pi-power-off',
+    command: event => {
+      window.location.hash = '#/apps'
+    }
+  }
+]
+
 const Routes = props => (
   <Router {...props}>
     <div>
-      <Route path="/" exact component={AppInit} />
-      <Route path="/apps" component={AppsContainer} />
-      <Route path="/authent" component={AuthentContainer} />
-      <Route path="/technos" component={TechnosContainer} />
-      <Route path="/flows" component={FlowsContainer} />
-      <Route path="/flowchart" component={FlowChart} />
+      <Menubar model={menuItems}>
+        <Button
+          label='Logout'
+          icon='pi pi-power-off'
+          style={{ marginLeft: 4 }}
+          onClick={() => {
+            document.cookie =
+              'atk=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;'
+            window.location.hash = '#/authent'
+          }}
+        />
+      </Menubar>
+
+      <Switch>
+        <Route path='/' exact component={AppInit} />
+        <Route path='/apps' component={AppsContainer} />
+        <Route path='/authent' component={AuthentContainer} />
+        <Route path='/technos' component={TechnosContainer} />
+        <Route path='/flows' component={FlowsContainer} />
+        <Route path='/flowchart' component={FlowChart} />
+      </Switch>
     </div>
   </Router>
 )
